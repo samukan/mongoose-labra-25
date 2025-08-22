5443eeae8a280edbe3ab45deb7ebc26dfbed1e2c
Samu Kangas <samu.kangas@metropolia.fi>
2025-08-22 10:46:06 +0300

    Testattu yhteys databaseen

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,15 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import CustomError from '../classes/CustomError';
 dotenv.config();
 
 const mongoConnect = async () => {
-  // Connect to MongoDB
+  if (!process.env.DB_URl) {
+    throw new CustomError('Database url not found in .env file', 500);
+  }
+  const connection = await mongoose.connect(process.env.DB_URl);
+  console.log('DB connected successfully');
+  return connection;
 };
 
 export default mongoConnect;
